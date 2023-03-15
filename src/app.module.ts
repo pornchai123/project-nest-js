2ac160b5a61b316d88a7b1d2362979af2f39ee00
nice_pi <53696374+nixce29@users.noreply.github.com>
2023-03-15 22:36:06 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mariadb',
+            host: 'localhost',
+            port: 8081,
+            username: 'root',
+            password: 'root',
+            database: 'test',
+            entities: [],
+            synchronize: true,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private dataSource: DataSource) { }
+}
